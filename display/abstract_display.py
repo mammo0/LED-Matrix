f3ce7cd85a4dc95b9ad15eb16fd9e376bbb5b561
mammo0 <marc.ammon@hotmail.de>
2020-12-22 11:04:29 +0100

    removed license text from files

--- display/abstract_display.py
+++ display/abstract_display.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-
-# RibbaPi - APA102 LED matrix controlled by Raspberry Pi in python
-# Copyright (C) 2016  Christoph Stahl
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 This module is the abstract representation of a pixel matrix display.
 """
