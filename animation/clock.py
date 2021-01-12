289ee11f094de5d3afba58db4f82760ec2de3ecf
mammo0 <marc.ammon@hotmail.de>
2021-01-12 13:52:55 +0100

    some refactoring

--- animation/clock.py
+++ animation/clock.py
@@ -23,47 +23,49 @@ class ClockAnimation(AbstractAnimation):
 
         self.background = Image.new("RGB", (width, height), background_color)
 
-        self.middle_x = int(width / 2)
-        self.middle_y = int(height / 2)
-        self.max_hand_length = min([self.middle_x, round(width / 2),
-                                    self.middle_y, round(height / 2)])
+        self.analog_middle_x = int(width / 2)
+        self.analog_middle_y = int(height / 2)
+        self.analog_max_hand_length = min([self.analog_middle_x, round(width / 2),
+                                           self.analog_middle_y, round(height / 2)])
 
-    def minute_point(self, minute):
+    def analog_minute_point(self, minute):
         minute %= 60
         angle = 2*math.pi * minute/60 - math.pi/2
 
-        x = int(self.middle_x + self.max_hand_length * math.cos(angle))
-        y = int(self.middle_y + self.max_hand_length * math.sin(angle))
+        x = int(self.analog_middle_x + self.analog_max_hand_length * math.cos(angle))
+        y = int(self.analog_middle_y + self.analog_max_hand_length * math.sin(angle))
 
         return (x, y)
 
-    def hour_point(self, hour):
+    def analog_hour_point(self, hour):
         hour %= 12
         angle = 2*math.pi * hour/12 - math.pi/2
-        length = int(self.max_hand_length / 2)
+        length = int(self.analog_max_hand_length / 2)
 
-        x = int(self.middle_x + length * math.cos(angle))
-        y = int(self.middle_y + length * math.sin(angle))
+        x = int(self.analog_middle_x + length * math.cos(angle))
+        y = int(self.analog_middle_y + length * math.sin(angle))
 
         return (x, y)
 
-    def add_hour_minute_hands(self, image, hour, minute):
-        middle_point = (self.middle_x, self.middle_y)
-        draw = ImageDraw.Draw(image)
-        draw.line([middle_point,
-                   self.minute_point(minute)], fill=self.minute_color)
-        draw.line([middle_point,
-                   self.hour_point(hour)], fill=self.hour_color)
-        draw.point(middle_point, fill=self.divider_color)
+    def analog_create_clock_image(self, hour, minute):
+        middle_point = (self.analog_middle_x, self.analog_middle_y)
+
+        draw = ImageDraw.Draw(self.background)
+        draw.line([middle_point, self.analog_minute_point(minute)],
+                  fill=self.minute_color)
+        draw.line([middle_point, self.analog_hour_point(hour)],
+                  fill=self.hour_color)
+        draw.point(middle_point,
+                   fill=self.divider_color)
+
+        return self.background
 
     def animate(self):
         while self._running:
             if self.variant == "analog":
                 local_time = time.localtime()
-                image = self.background.copy()
-                self.add_hour_minute_hands(image,
-                                           local_time.tm_hour,
-                                           local_time.tm_min)
+                image = self.analog_create_clock_image(local_time.tm_hour,
+                                                       local_time.tm_min)
                 self.frame_queue.put(np.array(image).copy())
                 time.sleep(1)
             else:
