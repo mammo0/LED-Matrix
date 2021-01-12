2fd4f1b0636079b247cf9e6a05203cca6a7d960c
mammo0 <marc.ammon@hotmail.de>
2021-01-12 16:22:18 +0100

    added a digital clock

--- animation/clock.py
+++ animation/clock.py
@@ -75,17 +75,100 @@ class ClockAnimation(AbstractAnimation):
 
         return image
 
+    def digital_draw_digit(self, draw, digit, x, y, width, height, color):
+        if digit in [4, 5, 6, 7, 8, 9, 0]:
+            # draw left upper
+            point_begin = (x, y)
+            point_end = (x, y + self.middle_calculation(height))
+            draw.line([point_begin, point_end], fill=color)
+        if digit in [2, 6, 8, 0]:
+            # draw left lower
+            point_begin = (x, y + self.middle_calculation(height))
+            point_end = (x, y + height - 1)
+            draw.line([point_begin, point_end], fill=color)
+
+        if digit in [1, 2, 3, 4, 7, 8, 9, 0]:
+            # draw right upper
+            point_begin = (x + width - 1, y)
+            point_end = (x + width - 1, y + self.middle_calculation(height))
+            draw.line([point_begin, point_end], fill=color)
+        if digit in [1, 3, 4, 5, 6, 7, 8, 9, 0]:
+            # draw right lower
+            point_begin = (x + width - 1, y + self.middle_calculation(height))
+            point_end = (x + width - 1, y + height - 1)
+            draw.line([point_begin, point_end], fill=color)
+
+        if digit in [2, 3, 5, 6, 7, 8, 9, 0]:
+            # draw top
+            point_begin = (x, y)
+            point_end = (x + width - 1, y)
+            draw.line([point_begin, point_end], fill=color)
+        if digit in [2, 3, 5, 6, 8, 9, 0]:
+            # draw bottom
+            point_begin = (x, y + height - 1)
+            point_end = (x + width - 1, y + height - 1)
+            draw.line([point_begin, point_end], fill=color)
+        if digit in [2, 3, 4, 5, 6, 8, 9]:
+            # draw middle
+            point_begin = (x, y + self.middle_calculation(height))
+            point_end = (x + width - 1, y + self.middle_calculation(height))
+            draw.line([point_begin, point_end], fill=color)
+
+    def digital_create_clock_image(self, hour, minute, second):
+        hour_txt = str(hour).zfill(2)
+        minute_txt = str(minute).zfill(2)
+
+        image = self.background.copy()
+        draw = ImageDraw.Draw(image)
+
+        # char width: space between middle and right/left - 1 (space between chars)} / 2 (two chars for hour/minute)
+        char_width = (self.analog_middle_x - 1) / 2
+
+        # char height: matrix height - 2 pixel space
+        char_height = self.height - 2
+
+        # draw hours
+        hour_1_x = self.analog_middle_x - (2 * char_width + 1)
+        hour_2_x = hour_1_x + char_width + 1
+        self.digital_draw_digit(draw, int(hour_txt[0]),
+                                x=hour_1_x, y=1, width=char_width, height=char_height, color=self.hour_color)
+        self.digital_draw_digit(draw, int(hour_txt[1]),
+                                x=hour_2_x, y=1, width=char_width, height=char_height, color=self.hour_color)
+
+        # draw minutes
+        minute_1_x = self.width - (2 * char_width + 1)
+        minute_2_x = minute_1_x + char_width + 1
+        self.digital_draw_digit(draw, int(minute_txt[0]),
+                                x=minute_1_x, y=1, width=char_width, height=char_height, color=self.minute_color)
+        self.digital_draw_digit(draw, int(minute_txt[1]),
+                                x=minute_2_x, y=1, width=char_width, height=char_height, color=self.minute_color)
+
+        # draw the minute hour divider every two seconds
+        divider_space = int(char_height / 4)
+        if second % 2 == 0:
+            draw.point([self.analog_middle_x, self.analog_middle_y + divider_space],
+                       fill=self.divider_color)
+            draw.point([self.analog_middle_x, self.analog_middle_y - divider_space],
+                       fill=self.divider_color)
+
+        return image
+
     def animate(self):
         while self._running:
+            local_time = time.localtime()
+
             if self.variant == ClockVariant.analog:
-                local_time = time.localtime()
                 image = self.analog_create_clock_image(local_time.tm_hour,
                                                        local_time.tm_min)
                 self.frame_queue.put(np.array(image).copy())
                 time.sleep(1)
             elif self.variant == ClockVariant.digital:
                 # TODO: add digital clock
-                pass
+                image = self.digital_create_clock_image(local_time.tm_hour,
+                                                        local_time.tm_min,
+                                                        local_time.tm_sec)
+                self.frame_queue.put(np.array(image).copy())
+                time.sleep(1/30)
 
     @property
     def kwargs(self):
