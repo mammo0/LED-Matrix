56282cef070a6779fcccc7e9632e777ad6b2c7c1
mammo0 <marc.ammon@hotmail.de>
2021-01-18 16:19:33 +0100

    added index.html

---
+++ resources/http/templates/base.tpl
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>{{title}}</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+</head>
+
+<body>
+
+    {{!base}}
+
+</body>
+
+</html>
--- server/http_server.py
+++ server/http_server.py
@@ -1,7 +1,17 @@
+from bottle import template
+import bottle
+
+from common import RESOURCES_DIR
+from common.bottle import BottleCBVMeta, get
 from common.wsgi import CustomWSGIRefServer
 
 
-class HttpServer():
+# change bottle template path
+HTTP_RESOURCES_DIR = RESOURCES_DIR / "http"
+bottle.TEMPLATE_PATH = [(HTTP_RESOURCES_DIR / "templates").resolve()]
+
+
+class HttpServer(metaclass=BottleCBVMeta):
     def __init__(self, main_app, port=8080):
         self.__main_app = main_app
         self.__port = port
@@ -13,3 +23,7 @@ class HttpServer():
 
     def stop(self):
         self.__wsgi_server.stop()
+
+    @get("/")
+    def index(self):
+        return template("index")
