25d33fd76489bcb38ec85d137acc81b1294f9c4f
mammo0 <marc.ammon@hotmail.de>
2021-01-19 15:38:32 +0100

    fix for possible errors

--- resources/http/js/slider.js
+++ resources/http/js/slider.js
@@ -5,11 +5,13 @@ window.onload = function(){
         var slider = container.getElementsByClassName("slider")[0];
         var output = container.getElementsByClassName("slider_value")[0];
 
-        output.innerHTML = slider.value;  // Display the default slider value
+        if(output){
+            output.innerHTML = slider.value;  // Display the default slider value
 
-        // Update the current slider value (each time you drag the slider handle)
-        slider.oninput = function() {
-            output.innerHTML = this.value;
+            // Update the current slider value (each time you drag the slider handle)
+            slider.oninput = function() {
+                output.innerHTML = this.value;
+            }
         }
     }
 };
