880448070fb44726771d5e8ad6ce438747fded57
mammo0 <marc.ammon@hotmail.de>
2021-01-21 10:34:47 +0100

    added new enum like type structure

---
+++ common/structure.py
@@ -0,0 +1,148 @@
+import types
+
+
+def _is_function(x):
+        return isinstance(x, types.FunctionType) \
+            or isinstance(x, types.BuiltinFunctionType)
+
+
+class _StructureMeta(type):
+    def __new__(metacls, cls, bases, classdict):
+        # do not do this when initializing the main class
+        if cls not in ("Structure", "NestedStructure"):
+            # get all static defined variables
+            params = {k: v for k, v in classdict.items() if not (
+                k.startswith("_") or
+                _is_function(v) or
+                isinstance(v, property)
+            )}
+
+            # and remove them from the normal class dictionary
+            for key in params.keys():
+                classdict.pop(key)
+        else:
+            params = {}
+
+        # add new attributes
+        new_cls = super().__new__(metacls, cls, bases, classdict)
+        new_cls._params_map_ = params
+        new_cls._params_names_ = list(params)
+
+        # maybe some members of the new class are NestedStructures
+        # so set the name property of them
+        if cls not in ("Structure", "NestedStructure"):
+            _NestedStructureMeta._add_names(metacls, new_cls)
+
+        return new_cls
+
+    def __dir__(cls):
+        return (['__class__', '__doc__', '__module__'] +
+                cls._params_map_)
+
+    def _check_get(cls, name):
+        if name not in cls._params_map_:
+            raise AttributeError(name) from None
+
+    def __getattr__(cls, name):
+        cls._check_get(name)
+        return cls._params_map_[name]
+
+    def _check_set(cls, name):
+        params_map = cls.__dict__.get('_params_map_', {})
+        if name in params_map:
+            raise AttributeError('Cannot reassign members.')
+
+    def __setattr__(cls, name, value):
+        cls._check_set(name)
+        super().__setattr__(name, value)
+
+    def _check_del(cls, attr):
+        if attr in cls._params_map_:
+            raise AttributeError("%s: cannot delete member." % cls.__name__)
+
+    def __delattr__(cls, attr):
+        cls._check_del(attr)
+        super().__delattr__(attr)
+
+    def __iter__(cls):
+        return iter(cls._params_map_.items())
+
+    @property
+    def names(cls):
+        return cls._params_names_
+
+
+class Structure(metaclass=_StructureMeta):
+    def __getattr__(self, name):
+        _StructureMeta._check_get(type(self), name)
+        return self._params_map_[name]
+
+    def __iter__(self):
+        return iter(self._params_map_.items())
+
+
+class StructureROMixin():
+    def __setattr__(self, name, value):
+        _StructureMeta._check_set(type(self), name)
+        super().__setattr__(name, value)
+
+    def __delattr__(self, attr):
+        _StructureMeta._check_del(type(self), attr)
+        super().__delattr__(attr)
+
+
+class _NestedStructureMeta(_StructureMeta):
+    def __new__(metacls, cls, bases, classdict):
+        new_cls = _StructureMeta.__new__(metacls, cls, bases, classdict)
+
+        # the names should be already added by _StructureMeta
+        # so add only the parent
+        metacls._add_parent(metacls, new_cls)
+
+        return new_cls
+
+    def _add_names(cls, iterable):
+        # iterate over the structure
+        for name, value in iterable:
+            if (isinstance(value, NestedStructure) or
+                    issubclass(value, NestedStructure)):
+                # set the name property
+                setattr(value, NestedStructure.name_attr_name, name)
+
+    def _add_parent(cls, iterable):
+        # iterate over the structure
+        for _, child in iterable:
+            if (isinstance(child, NestedStructure) or
+                    issubclass(child, NestedStructure)):
+                # set the parent property
+                setattr(child, NestedStructure.parent_attr_name, iterable)
+
+    @property
+    def name(cls):
+        return getattr(cls, NestedStructure.name_attr_name, None)
+
+    @property
+    def parent(cls):
+        return getattr(cls, NestedStructure.parent_attr_name, None)
+
+
+class NestedStructure(Structure, metaclass=_NestedStructureMeta):
+    name_attr_name = "_name_"
+    parent_attr_name = "_parent_"
+
+    def __new__(cls, *args, **kwargs):
+        _NestedStructureMeta._add_names(cls, cls)
+
+        instance = super(NestedStructure, cls).__new__(cls)
+
+        _NestedStructureMeta._add_parent(cls, instance)
+
+        return instance
+
+    @property
+    def name(self):
+        return getattr(self, NestedStructure.name_attr_name, None)
+
+    @property
+    def parent(self):
+        return getattr(self, NestedStructure.parent_attr_name, None)
