mammo0/LED-Matrix

commit 9d6fed09f60176f408485074dc1644281304419f

removed classproperty animation_name from AbstractAnimationController

main.py
@@ -240,7 +240,7 @@ class Main(MainInterface):
 
             # during load of the saved scheduled animations, the ANIMATION_SETTINGS attribute is a dict
             # it must be converted to the respective class instead
-            animation = self.available_animations[entry.ANIMATION_SETTINGS["animation_name"]]
+            animation = self.available_animations[entry.ANIMATION_SETTINGS.animation_name]
             entry.ANIMATION_SETTINGS = animation.default_animation_settings(**entry.ANIMATION_SETTINGS)
 
             # add job to the scheduler
@@ -657,9 +657,10 @@ class AnimationController(threading.Thread):
 
         # use module names to identify the animations not the class names
         for _name, cls in animation_loader.plugins.items():
-            animations[cls.animation_name] = cls(width=self.__display_width, height=self.__display_height,
-                                                 frame_queue=self.__display_frame_queue,
-                                                 on_finish_callable=self.__on_animation_finished)
+            animation_controller = cls(width=self.__display_width, height=self.__display_height,
+                                       frame_queue=self.__display_frame_queue,
+                                       on_finish_callable=self.__on_animation_finished)
+            animations[animation_controller.default_animation_settings.animation_name] = animation_controller
 
         return animations
 
@@ -718,7 +719,8 @@ class AnimationController(threading.Thread):
         if self.__current_animation is not None:
             # but only if not a specific animation should be stopped
             if (event is not None and
-                    self.__current_animation.animation_name != event.event_settings.animation_settings.animation_name):
+                    self.__current_animation.default_animation_settings.animation_name !=
+                    event.event_settings.animation_settings.animation_name):
                 return
             self.__current_animation.stop_animation()
             self.__current_animation = None
@@ -777,7 +779,7 @@ class AnimationController(threading.Thread):
             return None
         # check if the current animation is the one that should be stopped
         if (animation_name is not None and
-                animation_to_stop.animation_settings.animation_name != animation_name):
+                animation_to_stop.default_animation_settings.animation_name != animation_name):
             # otherwise do nothing
             return None
 
@@ -811,7 +813,7 @@ class AnimationController(threading.Thread):
     def get_current_animation_name(self):
         cur_a = self.__current_animation
         if cur_a:
-            return cur_a.animation_name
+            return cur_a.default_animation_settings.animation_name
         else:
             return ""
 

animation/abstract.py
@@ -2,11 +2,9 @@
 This is the sceleton code for all animations.
 """
 
-from abc import abstractmethod, ABC, ABCMeta
+from abc import abstractmethod, ABC
 from threading import Thread, Event
 import time
-
-from simple_classproperty import ClasspropertyMeta, classproperty
 
 from common import eprint
 from common.color import Color
@@ -175,16 +173,9 @@ class AbstractAnimation(ABC, Thread):
     #     Return -1 for animations that do not have an intrinsic_duration. The
     #     basic idea is to let animations run for only a default amount of time.
     #     Longer animations should be supported by asking for their intrinsic
-    #     duration. Of course repeats must be set to 0 then."""
 
 
-class AbstractAnimationControllerMeta(ABCMeta, ClasspropertyMeta):
-    """
-    Dummy class for chaining meta classes
-    """
-
-
-class AbstractAnimationController(metaclass=AbstractAnimationControllerMeta):
+class AbstractAnimationController(ABC):
     def __init__(self, width, height, frame_queue, on_finish_callable):
         self.__width = width  # width of frames to produce
         self.__height = height  # height of frames to produce
@@ -194,10 +185,6 @@ class AbstractAnimationController(metaclass=AbstractAnimationControllerMeta):
         self.__animation_thread = None  # this variable contains the animation thread
 
         self.__animation_running = Event()
-
-    @classproperty
-    def animation_name(cls):
-        return cls.__module__.rpartition(".")[-1]
 
     @property
     @abstractmethod

resources/http/templates/animation/selector.tpl
@@ -18,8 +18,8 @@
 <div class="my-3"></div>
 <hr/>
 
-% for animation in animations.values():
-    <div id="animation_settings_{{animation.animation_name}}" class="{{"d-none" if animation.animation_name != current_animation_name else ""}}">
+% for animation_name, animation in animations.items():
+    <div id="animation_settings_{{animation_name}}" class="{{"d-none" if animation_name != current_animation_name else ""}}">
         % include("animation/settings.tpl", animation_settings=animation.animation_settings)
     </div>
 % end

resources/http/templates/animation/variant_upload.tpl
@@ -9,7 +9,7 @@
             <div class="card">
                 <div class="card-header" id="dynamic_variant_header_{{animation_name}}">
                     <h2 class="mb-0">
-                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#dynamic_variant_body_{{animation_name}}" aria-expanded="false" aria-controls="dynamic_variant_body_{{animation.animation_name}}">
+                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#dynamic_variant_body_{{animation_name}}" aria-expanded="false" aria-controls="dynamic_variant_body_{{animation_name}}">
                             {{animation_name.title()}}
                         </button>
                     </h2>

server/tpm2_net.py
@@ -46,7 +46,7 @@ class Tpm2NetServer(socketserver.UDPServer):
         if self.__last_time_received:
             if self.__last_time_received + self.__timeout < time.time():
                 # stop dummy animation
-                self.__main_app.stop_animation(self.__dummy_animation.animation_name)
+                self.__main_app.stop_animation(self.__dummy_animation.default_animation_settings.animation_name)
                 self.__last_time_received = None
                 self.__timeout_timer = None
                 self.__misbehaving = False
@@ -90,7 +90,7 @@ class Tpm2NetHandler(socketserver.BaseRequestHandler):
             # tell main_app that tpm2_net data is received
             if not self.__dummy_animation.is_running:
                 # use dummy animation, because the frame_queue gets filled here
-                self.server.main_app.start_animation(self.__dummy_animation.animation_name)
+                self.server.main_app.start_animation(self.__dummy_animation.default_animation_settings.animation_name)
             self.server.update_time()
 
             if packet_number == 0:

commit 895bd1146cd10576bf0117b3861bfb92b0e71f03

fixed loading error introduced by commit 9d6fed0

main.py
@@ -240,7 +240,7 @@ class Main(MainInterface):
 
             # during load of the saved scheduled animations, the ANIMATION_SETTINGS attribute is a dict
             # it must be converted to the respective class instead
-            animation = self.available_animations[entry.ANIMATION_SETTINGS.animation_name]
+            animation = self.available_animations[entry.ANIMATION_SETTINGS["animation_name"]]
             entry.ANIMATION_SETTINGS = animation.default_animation_settings(**entry.ANIMATION_SETTINGS)
 
             # add job to the scheduler