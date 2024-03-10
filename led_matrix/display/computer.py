a6fac2ec2d0a56845bdd7a06f7b012b8ca8960fd
mammo0 <marc.ammon@hotmail.de>
2024-03-10 12:43:17 +0100

    use an own thread for pygame

--- led_matrix/display/computer.py
+++ led_matrix/display/computer.py
@@ -1,29 +1,40 @@
+import queue
+from queue import Queue
+from threading import Thread
+from typing import Final
+
 import numpy as np
 import pygame
+from numpy.typing import NDArray
 from pygame import Vector2
 from pygame.color import Color
 from pygame.event import Event
 from pygame.rect import Rect
 from pygame.surface import Surface
+from pygame.time import Clock
 
 from led_matrix.config.settings import Settings
 from led_matrix.display.abstract import AbstractDisplay
 from led_matrix.main import MainController
 
+class _PygameDisplayThread(Thread):
+    MAX_FPS: Final[float] = 60.
 
-class Computer(AbstractDisplay):
-    def __init__(self, config: Settings) -> None:
-        super().__init__(config=config)
+    def __init__(self, width: int, height: int, margin: int, size: int) -> None:
+        super().__init__()
+
+        self.__width: int = width
+        self.__height: int = height
+        self.__margin: int = margin
+        self.__size: int = size
+
+        self.__display_queue: Queue[NDArray[np.uint8]] = Queue()
+        self.__last_frame: NDArray[np.uint8] | None = None
 
         self.__brightness: float = 100.
 
-        width: int = config.main.display_width
-        height: int = config.main.display_height
-        self.__margin: int = config.computer.margin
-        self.__size: int = config.computer.led_size
-
-        window_size: tuple[int, int] = (width * self.__size + (width + 1) * self.__margin,
-                                        height * self.__size + (height + 1) * self.__margin)
+        self.__window_size: tuple[int, int] = (width * size + (width + 1) * margin,
+                                               height * size + (height + 1) * margin)
 
         # these variables are used for simulating a LED
         # use this to set the amount of 'segments' we rotate our blend into
@@ -36,11 +47,8 @@ class Computer(AbstractDisplay):
         self.__led_gradient_background.b = self.__led_gradient_background.b // self.__circular_smoothness_steps
         self.__led_gradient_background.a = self.__led_gradient_background.a // self.__circular_smoothness_steps
 
-        pygame.init()
-        self.__surface: Surface = pygame.display.set_mode(window_size)
-        pygame.display.set_caption(f"LED-Matrix {width}x{height}")
-
-        self.show()
+        # clock to handle fps
+        self.__clock: Clock = Clock()
 
     def __get_led_surface(self, led_color: Color) -> Surface | None:
         if (led_color.r, led_color.g, led_color.b) == (0, 0, 0):
@@ -78,32 +86,19 @@ class Computer(AbstractDisplay):
 
         return led_surface
 
-    def set_brightness(self, brightness: int) -> None:
-        # expecting float brightness 0 .. 1.0
-        self.__brightness = brightness / 100
+    def __draw_frame(self, surface: Surface, frame: NDArray[np.uint8]) -> None:
+        surface.fill(color=Color(0, 0, 0))
 
-    def show(self, gamma: bool=False) -> None:
-        event: Event
-        try:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    # stop everything
-                    MainController.quit()
-                    return
-        except pygame.error:
-            # this error is normally thrown if we are already shutting down...
-            return
+        # lock brightness value
+        frame_brightness: float = self.__brightness
 
-        self.__surface.fill(color=Color(0, 0, 0))
-
-        it: np.nditer = np.nditer([self.frame_buffer[..., 0],
-                                   self.frame_buffer[..., 1],
-                                   self.frame_buffer[..., 2]], flags=['multi_index'])
+        it: np.nditer = np.nditer([frame[..., 0],
+                                   frame[..., 1],
+                                   frame[..., 2]], flags=['multi_index'])
         while not it.finished:
-            color: Color = Color(int(it[0] * self.__brightness),
-                                 int(it[1] * self.__brightness),
-                                 int(it[2] * self.__brightness))
+            color: Color = Color(int(it[0] * frame_brightness),
+                                 int(it[1] * frame_brightness),
+                                 int(it[2] * frame_brightness))
 
             # get the LED surface
             led_surface: Surface | None = self.__get_led_surface(led_color=color)
@@ -119,10 +114,78 @@ class Computer(AbstractDisplay):
                                            y=(self.__margin + self.__size) * row + self.__margin)
 
                 # add the LED surface to the main one
-                self.__surface.blit(led_surface,
-                                    dest=led_pos)
+                surface.blit(led_surface, dest=led_pos)
 
             it.iternext()
 
-        pygame.display.update()
-        # pygame.event.clear()
+    def display_frame(self, frame: NDArray[np.uint8]) -> None:
+        # queue the frame
+        self.__display_queue.put(frame)
+
+    def set_brightness(self, brightness: int) -> None:
+        # expecting float brightness 0 .. 1.0
+        self.__brightness = brightness / 100
+
+        # re-add the last frame to the display queue on brightness change
+        # this triggers an immediate redraw of the frame
+        if self.__last_frame is not None:
+            self.__display_queue.put(self.__last_frame)
+
+    def run(self) -> None:
+        # pygame must be initialized in the thread that updates the display
+        pygame.init()
+        surface: Surface = pygame.display.set_mode(self.__window_size)
+        pygame.display.set_caption(f"LED-Matrix {self.__width}x{self.__height}")
+
+        # start the main loop
+        while True:
+            try:
+                event: Event
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        # stop everything
+                        MainController.quit()
+                        return
+            except pygame.error:
+                # this error is normally thrown if we are already shutting down...
+                return
+
+            try:
+                # preserve the last frame to be able to redraw it
+                # needed on brightness change
+                self.__last_frame = self.__display_queue.get(block=False)
+            except queue.Empty:
+                # just continue here
+                pass
+            else:
+                self.__draw_frame(surface=surface,
+                                  frame=self.__last_frame)
+
+                self.__display_queue.task_done()
+
+            # draw the game screen
+            pygame.display.update()
+
+            # limit the FPS by sleeping for the remainder of the frame time
+            self.__clock.tick(_PygameDisplayThread.MAX_FPS)
+
+
+class Computer(AbstractDisplay):
+    def __init__(self, config: Settings) -> None:
+        super().__init__(config=config)
+
+        self.__pygame_thread: _PygameDisplayThread = _PygameDisplayThread(width=config.main.display_width,
+                                                                          height=config.main.display_height,
+                                                                          margin=config.computer.margin,
+                                                                          size=config.computer.led_size)
+
+        # start the pygame thread
+        self.__pygame_thread.start()
+
+    def set_brightness(self, brightness: int) -> None:
+        # forward brightness to pygame thread
+        self.__pygame_thread.set_brightness(brightness)
+
+    def show(self, gamma: bool=False) -> None:
+        self.__pygame_thread.display_frame(frame=self.frame_buffer)
